8cc13cc1a3c41ed98c23ca7a6abd2d2cff8f82cf
Jana Moudra <jana.moudra@gmail.com>
2014-11-03 11:54:36 +0100

    Added note app

---
+++ dart_codelab/web/dart_codelab.dart
@@ -0,0 +1,114 @@
+import 'dart:html';
+import 'dart:convert';
+
+InputElement noteInput;
+UListElement notesWrapper;
+AnchorElement notesDeleteAll;
+List notes;
+final String NOTES_LIST = "notesList";
+
+void main() {
+  noteInput = querySelector("#note_add_form");
+  noteInput.onChange.listen(createNote);  
+  
+  notesWrapper = querySelector("#notes_wrapper");
+  
+  notesDeleteAll = querySelector("#notes_delete_all");
+  notesDeleteAll.onClick.listen(deleteAll); 
+  
+  loadFromLoacalStorage();
+}
+
+void createNote(Event event) {
+  String noteText = noteInput.value;
+  if (noteText.isNotEmpty) {
+    addNote(noteText);
+  }
+}
+
+void addNote(String noteText) {  
+  notes.add(new Note(noteText)); 
+  addNoteElement(noteText);
+  noteInput.value = "";
+  saveToLocalStorage();
+}
+
+void addNoteElement(String noteText) {
+  LIElement note = new LIElement();
+  note.text = noteText; 
+  addDeleteNoteButton(note);
+  notesWrapper.append(note);
+}
+
+void addDeleteNoteButton(LIElement note) {
+  AnchorElement anchor = new AnchorElement(href: "#");
+  anchor
+    ..text = "Click"
+    ..onClick.listen(deleteNote);
+  note.append(anchor);
+}
+
+void deleteNote(Event event) {
+  event.preventDefault();
+  LIElement actualNote = ((event.target as AnchorElement).parent as LIElement);  
+  int actualIndex = notesWrapper.children.indexOf(actualNote);
+  notes.removeAt(actualIndex);  
+  actualNote.remove();  
+  saveToLocalStorage();
+}
+
+void deleteAll(Event event) {
+  event.preventDefault();
+  notes.clear();  
+  notesWrapper.children.clear();
+  saveToLocalStorage();
+}
+
+void addNotes() {
+  for (Note note in notes) {
+    addNoteElement(note.text);
+  }
+}
+
+void loadFromLoacalStorage() {
+  if (window.localStorage[NOTES_LIST] != null) {
+    notes = Note.fromJson(window.localStorage[NOTES_LIST]);
+    addNotes();
+  } else {
+    notes = new List();
+  }
+}
+
+void saveToLocalStorage() {
+  window.localStorage[NOTES_LIST] = Note.toJson(notes);
+}
+
+
+class Note {
+  String text;
+  
+  Note(this.text);
+  
+  static List fromJson(String jsonString) {
+    List notes = new List();    
+    List notesJsonList = JSON.decode(jsonString);
+    
+    for (Map noteJsonMap in notesJsonList) {
+      Note note = new Note(noteJsonMap["text"]);
+      notes.add(note);
+    }
+    return notes;
+  }
+  
+  static String toJson(List notes) {
+    List notesJsonList = new List();
+    
+    for (Note note in notes) {
+      Map noteJsonMap = new Map();
+      noteJsonMap["text"] = note.text;
+      notesJsonList.add(noteJsonMap);
+    }
+    
+    return JSON.encode(notesJsonList);
+  }
+}
